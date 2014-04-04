2ced366b54961ce7cbb8dfcdf0251fcd5d34792f
colinmwalsh@gmail.com <colinmwalsh@gmail.com@78898d3d-0ae4-eff4-7dcb-c4e3aacf88b4>
2014-04-04 13:35:25 +0000

    Version 1 contains just a restful web service that returns data stored locally. GET /api/client & GET /api/client/id

---
+++ trunk/Code/CA2MiniProject/Controllers/ClientController.cs
@@ -0,0 +1,84 @@
+﻿// a RESFul service which reports Client information
+// By Louise Foley X number here & Collin Walsh X00115912
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using CA2MiniProject.Models;                  // Client information model class
+
+
+namespace CA2MiniProject.Controllers
+{
+    public class ClientController : ApiController
+         {
+        /*
+        * GET /api/client                  get client information                      GetAllClientInfo()
+        * GET /api/client/name             get client information for name             GetClientInfoForCity(name)
+        */
+
+        private List<ClientInfo> client;
+
+        // initialise the client collection, stateless
+        public ClientController()
+        {
+            client = new List<ClientInfo>() 
+                { 
+                    new ClientInfo { ID = "x9864", Name = "Peter", Age = 22, Phone_Number = "087451237", Email = "peter@gmail.com", Post_Code = "D24", Gender = "Male", Looking_For = "Female", Interest_1 = "Sport", Interest_2 = "Music", Interest_3 = "Hiking" },
+                    new ClientInfo { ID = "x9865", Name = "Frank", Age = 23, Phone_Number = "087451236", Email = "Frank@gmail.com", Post_Code = "D24", Gender = "Male", Looking_For = "Female", Interest_1 = "Sport", Interest_2 = "Music", Interest_3 = "Hiking" },
+                };
+        }
+
+        // GET api/client
+        public IEnumerable<ClientInfo> GetAllClientInfo()
+        {
+            return client;                                                     
+        }
+
+        // GET api/client/Dublin or api/client?city=Dublin
+        public ClientInfo GetClientInfoForCity(String id)
+        {
+            // LINQ query, find matching city (case-insensitive) or default value (null) if none matching
+            ClientInfo clientSearch = client.FirstOrDefault(w => w.ID.ToUpper() == id.ToUpper());
+            if (clientSearch == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);       // translated into a http response status code 404
+            }
+            return clientSearch;                                                 
+        }
+
+    }
+
+    //public class ClientController : ApiController
+    //{
+    //    // GET api/values
+    //    public IEnumerable<string> Get()
+    //    {
+    //        return new string[] { "value1", "value2" };
+    //    }
+
+    //    // GET api/values/5
+    //    public string Get(int id)
+    //    {
+    //        return "value";
+    //    }
+
+    //    // POST api/values
+    //    public void Post([FromBody]string value)
+    //    {
+    //    }
+
+    //    // PUT api/values/5
+    //    public void Put(int id, [FromBody]string value)
+    //    {
+    //    }
+
+    //    // DELETE api/values/5
+    //    public void Delete(int id)
+    //    {
+    //    }
+    //}
+}
---
+++ trunk/Code/CA2MiniProject/Models/ClientModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CA2MiniProject.Models
+{
+    public class ClientInfo
+    {
+        // The ID 
+        [Required(ErrorMessage = "Not a Valid ID")]
+        public String ID
+        {
+            get;
+            set;
+        }
+        // Client Name
+        [Range(-50, 50, ErrorMessage = "Not a Valid  Name")]
+        public String Name
+        {
+            get;
+            set;
+        }
+        // Client Age
+        [Range(0, 200, ErrorMessage = "Not a Valid Age")]
+        public int Age
+        {
+            get;
+            set;
+        }
+        // Client Phone_Number    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Phone_Number
+        {
+            get;
+            set;
+        }
+        // Client Email    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Email
+        {
+            get;
+            set;
+        }
+        // Client Post_Code    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Post_Code
+        {
+            get;
+            set;
+        }
+        // Client Gender    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Gender
+        {
+            get;    
+            set;
+        }
+        // Client Looking_For    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Looking_For
+        {
+            get;
+            set;
+        }
+        // Client Interest_1    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Interest_1
+        {
+            get;
+            set;
+        }
+        // Client Interest_2    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Interest_2
+        {
+            get;
+            set;
+        }
+        // Client Interest_1    
+        [Required(ErrorMessage = "Invalid Conditions")]
+        public String Interest_3
+        {
+            get;
+            set;
+        }
+    }
+}
