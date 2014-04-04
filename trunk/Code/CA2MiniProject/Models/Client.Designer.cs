c752133ec747b9bd8e329bd395e95cda7bf57df3
colinmwalsh@gmail.com <colinmwalsh@gmail.com@78898d3d-0ae4-eff4-7dcb-c4e3aacf88b4>
2014-04-04 20:17:08 +0000

    Ver2. tried to get it to use Entity Framework.

---
+++ trunk/Code/CA2MiniProject/Models/Client.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'G:\College Work\EAM\Term 2\CA2 Mini Project\Code\CA2MiniProject\Models\Client.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
