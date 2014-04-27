8806e7f7bcade37ec18a1509de7114fdaf725ed2
louisefoley@gmail.com <louisefoley@gmail.com@78898d3d-0ae4-eff4-7dcb-c4e3aacf88b4>
2014-04-27 16:09:43 +0000

    Minor edits to add local dbContext and to eliminate bugs. Its looking like we might need to restart from scratch with scaffolding and maybe use code or model first.

--- trunk/Code/CA2MiniProject/Controllers/HomeController.cs
+++ trunk/Code/CA2MiniProject/Controllers/HomeController.cs
@@ -10,62 +10,62 @@ namespace CA2MiniProject.Controllers
     public class HomeController : Controller
     {
         // strongly typed view to test view 
-        public ActionResult List()
-        {
-            var user = new List<User>
-    {
-        new User { 
-            ID = "D1234", 
-            Name = "John Dunn", 
-            Age = 25, 
-            Phone_Number = "0872582466",
-            Email = "JohnDunn@Hotmil.com",
-            Post_Code="Carlow",
-            Gender="Male",
-            Looking_For="Female",
-            Interest_1="Music",
-            Interest_2="Cooking",
-            Interest_3="Sport"
-        },
-        new User { 
-            ID = "D2345", 
-            Name = "Ann Quinn", 
-            Age = 28, 
-            Phone_Number = "0872582422",
-            Email = "Quinner@Hotmil.com",
-            Post_Code="Waterford",
-            Gender="Female",
-            Looking_For="Male",
-            Interest_1="Music",
-            Interest_2="Cooking",
-            Interest_3="Technology"
-        },
+        //public ActionResult List()
+        //{
+    //        var user = new List<User>
+    //{
+    //    new User { 
+    //        ID = "D1234", 
+    //        Name = "John Dunn", 
+    //        Age = 25, 
+    //        Phone_Number = "0872582466",
+    //        Email = "JohnDunn@Hotmil.com",
+    //        Post_Code="Carlow",
+    //        Gender="Male",
+    //        Looking_For="Female",
+    //        Interest_1="Music",
+    //        Interest_2="Cooking",
+    //        Interest_3="Sport"
+    //    },
+    //    new User { 
+    //        ID = "D2345", 
+    //        Name = "Ann Quinn", 
+    //        Age = 28, 
+    //        Phone_Number = "0872582422",
+    //        Email = "Quinner@Hotmil.com",
+    //        Post_Code="Waterford",
+    //        Gender="Female",
+    //        Looking_For="Male",
+    //        Interest_1="Music",
+    //        Interest_2="Cooking",
+    //        Interest_3="Technology"
+    //    },
         
-    };
+    //};
 
-            // pass in message to view also
-            ViewBag.Words = new List<string> { "User:"};
-            return View(user);
-        }
+    //        // pass in message to view also
+    //        ViewBag.Words = new List<string> { "User:"};
+    //        return View(user);
+    //    }
 
 
-        // Get to return existing person from ID /Home/Find
-        [HttpGet]
-        // find a user based on Id
-        public ActionResult Find(int ID = 0)
-        {
-           var user = UserInfo.Find(ID);
-            if (User == null)
-            {
-                return HttpNotFound();
-            }
+        //// Get to return existing person from ID /Home/Find
+        //[HttpGet]
+        //// find a user based on Id
+        //public ActionResult Find(int ID = 0)
+        //{
+        //    var user = UserInfo.Find(ID);
+        //    if (User == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
 
-            return View(User);
-        }
+        //    return View(User);
+        //}
 
 
         // Post to add a new person to db  /Home/EditPage
-        [HttpPost]
+        //[HttpPost]
         public ActionResult EditPage(User user)
         {
             try
---
+++ trunk/Code/CA2MiniProject/Models/CA2MiniProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CA2MiniProject.Models
+{
+    public class CA2MiniProjectContext :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
