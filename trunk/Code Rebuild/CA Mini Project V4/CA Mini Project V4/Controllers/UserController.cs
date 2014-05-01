d52fe11e963e70582d5a8fdf865681560b01a33d
colinmwalsh@gmail.com <colinmwalsh@gmail.com@78898d3d-0ae4-eff4-7dcb-c4e3aacf88b4>
2014-05-01 17:43:32 +0000

    Rebuilt the Project using some MVC code from older version, but without local DB etc changed the database connection so it now connects to User

---
+++ trunk/Code Rebuild/CA Mini Project V4/CA Mini Project V4/Controllers/UserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using CA_Mini_Project_V4.Models;
+
+namespace CA_Mini_Project_V4.Controllers
+{
+    public class UserController : ApiController
+    {
+        /*
+       * GET /api/user                  get all users information                      GetAllUserInfo()
+       * GET /api/user/id               get user details for id                  GetUserNumber(id)
+       */
+
+
+        private AzureDatabaseEntities db = new AzureDatabaseEntities();
+
+        // GET api/User
+        public IEnumerable<User> GetUsers()
+        {
+            return db.Users.AsEnumerable();
+        }
+
+        // GET api/User/x09782
+        public User GetUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return user;
+        }
+
+        //the below code was auto generated and can be deleted. leaving it in place for the moment ifn case we want to add more funcionality
+
+        // PUT api/User/5
+        public HttpResponseMessage PutUser(string id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != user.ID)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/User
+        public HttpResponseMessage PostUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = user.ID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/User/5
+        public HttpResponseMessage DeleteUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Users.Remove(user);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
