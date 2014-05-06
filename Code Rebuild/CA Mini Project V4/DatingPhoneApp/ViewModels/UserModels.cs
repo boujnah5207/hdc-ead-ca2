3f4f94ba4dd14e5a7ae91e1034c85aabec9adbf6
louisefoley <LouiseFoley@gmail.com>
2014-05-06 00:05:24 +0000

    Phone app test added

--- Code Rebuild/CA Mini Project V4/DatingPhoneApp/ViewModels/UserModels.cs
+++ Code Rebuild/CA Mini Project V4/DatingPhoneApp/ViewModels/UserModels.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 using System.Linq;
 using System.Xml;
@@ -12,7 +11,10 @@ using System.Web;
 
 namespace CA_Mini_Project_V4.Models
 {
+
+
     // a class to hold the details of the dating website users
+   
     public partial class UserInfo
     {
 
@@ -20,7 +22,7 @@ namespace CA_Mini_Project_V4.Models
         {
             get
             {
-                return new String[] { "Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois", "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow" };
+                return new String[] { "Dublin 1 ", "Dublin 2", "Dublin 3", "Dublin 4", "Dublin 5", "Dublin 6", "Dublin 7", "Dublin 8", "Dublin 9", "Dublin 10", "Dublin 11", "Dublin 12", "Dublin 13", "Dublin 14", "Dublin 15", "Dublin 16", "Dublin 18", "Dublin 20", "Dublin 22", "Dublin 24", "Kildare", "Meath", "Wicklow", "Other"};
             }
         }
         public static string[] Interest1Descriptions
@@ -47,72 +49,119 @@ namespace CA_Mini_Project_V4.Models
             }
         }
 
-
+        // User ID 
+        //[Required(ErrorMessage = "Not a Valid ID")] // not null or empty string, not enforced automatically
         public string ID
         {
             get;
             set;
         }
-        
+        //User Name
+        //[Range(-50, 50, ErrorMessage = "Not a Valid Name")]
         public String Name
         {
             get;
             set;
         }
-
+        // User Age
+        //[Range(18, 200, ErrorMessage = "Not a Valid Age")]
+        private int age;
         public int Age
         {
-            get;
-            set;
+            get
+            {
+                return age;
+            }
+            set
+            {
+                // validate input
+                if (value > 18)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("User must be over 18");
+                }
+            }
         }
-   
+
+         
+        // User Phone_Number    
+        //[Required(ErrorMessage = "Phone number must be entered")]
+        //[Display(Name = "Phone Number")]
         public String Phone_Number
         {
             get;
             set;
         }
-     
+        //// User Email    
+        //[EmailAddress]
+        //[Required]
+        //[RegularExpression(@".*[@].*[\\.].*", ErrorMessage = "Must contain @ and .")]
         public String Email
         {
             get;
             set;
         }
-    
+        // user Post_Code    
+        //[Required(ErrorMessage = "Invalid Conditions")]
+        //[Display(Name = "Post Code")]
         public String Post_Code
         {
             get;
             set;
         }
-    
+        // User Gender    
+        //[Required(ErrorMessage = "Invalid Conditions")]
         public String Gender
         {
             get;
             set;
         }
- 
-      
+        // User Looking_For    
+        //[Required(ErrorMessage = "Invalid Conditions")]
+        //[Display(Name = "Looking For")]
         public String Looking_For
         {
             get;
             set;
         }
-       
+        // User Interest_1    
+        //[Required(ErrorMessage = "Invalid Conditions")]
+        //[Display(Name = "1st Interest")]
         public String Interest_1
         {
             get;
             set;
         }
-   
+        // User Interest_2    
+        //[Required(ErrorMessage = "Invalid Conditions")]
+        //[Display(Name = "2nd interest")]
         public String Interest_2
         {
             get;
             set;
         }
-
+        // User Interest_3    
+        //[Required(ErrorMessage = "Invalid Conditions")]
+        //[Display(Name = "3rd Interest")]
         public String Interest_3
         {
             get;
             set;
         }
+
+        //The below is commented out as it is producing errors when used in the PhoneApp
+        //private AzureDatabaseEntities db = new AzureDatabaseEntities();
+
+        //public IEnumerable<User> FindMatch(string interest_1)
+        //{
+        //    // query Users with the same Interest
+        //    var myMatch = from x in db.Users
+        //                  where x.Interest_1 == interest_1
+        //                  select x;
+        //    return myMatch;
+        //}
     }
 }
---
+++ Code Rebuild/CA Mini Project V4/PhoneAppTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using CA_Mini_Project_V4.Models;
+
+namespace PhoneAppTest
+{
+    [TestClass]
+    public class PhoneAppTests
+    {
+        private UserInfo user = new UserInfo();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            user = new UserInfo();
+        }
+
+        [TestMethod]
+        public void AlwaysTrue()
+        {
+            Assert.IsTrue(true, "this method will always pass");
+        }
+        
+        // test that the mainPage is create without errors
+        [TestMethod]
+        public void TestMainPageNotNull()
+        {
+            DatingPhoneApp.MainPage CPage = new DatingPhoneApp.MainPage();
+
+            // Assert
+            Assert.IsNotNull(CPage);
+        }
+
+        //test the returned data
+        [TestMethod]
+        public void CheckListForDataFirstTime()
+        {
+            DatingPhoneApp.MainPage CPage = new DatingPhoneApp.MainPage();
+            Assert.AreEqual(CPage.Content, 0);
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            user = null;
+        }
+    }
+}
